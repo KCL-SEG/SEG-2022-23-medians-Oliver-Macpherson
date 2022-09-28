7e317136222d61bd8910df1c2fa4f23adfe88894
Oliver Macpherson <oliver.macpherson@kcl.ac.uk>
2022-09-28 12:27:28 +0100

    Add median calculation to median.py

--- median.py
+++ median.py
@@ -9,4 +9,19 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+median = 0
+if(isinstance(numbers,int)):
+    median = numbers
+else:
+    sorted = sorted(numbers)
+    if (len(sorted)%2)!=0:
+        median = sorted[len(sorted)//2]
+    elif len(sorted)%2==0:
+        lmid = len(sorted)//2
+        umid = lmid-1
+        median = (sorted[lmid]+sorted[umid])/2
+    else:
+        print('Something weird has happened.')
+
+print(median)
